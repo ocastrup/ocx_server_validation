5cc005519fed2de7a0ef191d14a59e2254aaeeae
oca <ole.christian.astrup@dnvgl.com>
2020-01-27 10:08:18 +0100

    First version of validator ready for deplyment

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ocx-server-validation',
+    version='0.1',
+    packages=['SchemaParser'],
+    url='',
+    license='',
+    author='oca',
+    author_email='ole.christian.astrup@dnvgl.com',
+    description='OCX schema validation '
+)
